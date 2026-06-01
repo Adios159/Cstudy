4c5a9deb94170d224d7fbc8b805cf97e7d8e46f8
Adios159 <imadios@naver.com>
2026-06-01 22:16:49 +0900

    자료구조 기말고사 공부

---
+++ DataStructure/Lliststring.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char name[100];
+} element;
+
+typedef struct ListNode {
+    element data;
+    struct ListNode *link;
+} ListNode;
+
+void error(char *message) {
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+
+ListNode* insert_firat(ListNode *head, element value) {
+    ListNode *p = (ListNode*)malloc(sizeof(ListNode));
+    p->data = value;
+    p->link = head;
+    head = p;
+    return head;
+}
+
+void print_list(ListNode *head) {
+    for(ListNode *p = head; p != NULL; p = p->link) {
+        printf("%s->", p->data.name);
+    }
+     printf("NULL \n");
+}
+
+int main(void) {
+    ListNode *head = NULL;
+    element data;
+
+    strcpy(data.name, "apple");
+    head = insert_firat(head, data);
+    print_list(head);
+
+    strcpy(data.name, "banana");
+    head = insert_firat(head, data);
+    print_list(head);
+
+    strcpy(data.name, "kiwi");
+    head = insert_firat(head, data);
+    print_list(head);
+
+    return 0;
+}
