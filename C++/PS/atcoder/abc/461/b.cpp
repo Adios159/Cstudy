2743f3b2d0b7d37afa32c6f71171087aefeb4be8
imadios <imadios@naver.com>
2026-06-17 23:16:56 +0900

    폴더이름 업데이트

---
+++ C++/PS/atcoder/abc/461/b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, cnt = 0;
+    cin >> N;
+    vector<int> A(N + 1), B(N + 1);
+
+    for(int i = 1; i <= N; i++) {
+        cin >> A[i];
+    }
+    for(int i = 1; i <= N; i++) {
+        cin >> B[i];
+    }
+    for(int i = 1; i <= N; i++) {
+        if(B[A[i]] != i) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
