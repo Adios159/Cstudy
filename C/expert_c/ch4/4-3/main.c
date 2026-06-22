d8104030b8403180c90d86348d1b4d5142fd1545
Adios159 <imadios@naver.com>
2026-06-22 09:35:50 +0000

    expert C 6/22

---
+++ C/expert_c/ch4/4-3/main.c
@@ -0,0 +1,10 @@
+int global_var1;
+int global_var2;
+int global_var3 = 0;
+
+char global_var4 = 'A';
+double global_var5 = 4.5; 
+
+int main(int argc, char** argv) {
+    return 0;
+} 
---
+++ C/expert_c/ch4/4-4/main.c
@@ -0,0 +1,7 @@
+int x = 33;
+int y = 0x12151367;
+char z[6] = "ABCDE";
+
+int main(int argc, char** argv) {
+    return 0;
+}  
