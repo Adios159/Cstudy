59f1310c4868882ce0f4f2731a980dd72098fbbf
Adios159 <imadios@naver.com>
2026-06-30 18:04:32 +0900

    이것이 자료구조&알고리즘이다 이중 연결리스트

---
+++ C/DataStructure&Algorithm/DataStructure/ThisIsD&A/LinkedList/DList/DList.c
@@ -0,0 +1,93 @@
+#include "DList.h"
+
+Node* create_node(element data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    return newNode;
+}
+
+void destroy_node(Node* node) {
+    free(node);
+}
+
+void append_node(Node** head, Node* newNode) {
+    if((*head) == NULL) {
+        *head = newNode;
+    }
+    else {
+        Node* tail = (*head);
+        while(tail->next != NULL) {
+            tail = tail->next;
+        }
+        tail->next = newNode;
+        newNode->prev = tail;
+    }
+}
+
+void insert_after(Node* current, Node* newNode) {
+    newNode->next = current->next;
+    newNode->prev = current;
+    if(current->next != NULL) {
+        current->next->prev = newNode;
+        current->next = newNode;
+    }
+}
+
+void remove_node(Node** head, Node* remove) {
+    if(*head == remove) {
+        *head = remove->next;
+        if((*head) != NULL){
+            (*head)->prev = NULL;
+        }
+        remove->prev = NULL;
+        remove->next = NULL;
+    }
+    else {
+        Node* temp = remove;
+        if(remove->prev != NULL) {
+            remove->prev->next = temp->next;
+        }
+        if(remove->next != NULL) {
+            remove->next->prev = temp->prev;
+        }
+        remove->prev = NULL;
+        remove->next = NULL;
+    }
+}
+
+Node* get_node_at(Node* head, int location) {
+    Node* current = head;
+    while(current != NULL && (--location) >= 0) {
+        current = current->next;
+    }
+    return current;
+}
+
+int get_node_count(Node* head) {
+    unsigned int count = 0;
+    Node* current = head;
+    while(current != NULL) {
+        current = current->next;
+        count++;
+    }
+    return count;
+}
+
+void print_node(Node* _node) {
+    if(_node->prev == NULL) {
+        printf("Prev: NULL");
+    }
+    else {
+        printf("Prev: &d", _node->prev->data);
+        printf("Current: &d", _node->data);
+    }
+    if(_node->next == NULL) {
+        printf("Next: NULL\n");
+    }
+    else {
+        printf("Next: &d\n", _node->next->data);
+    }
+}
---
+++ C/DataStructure&Algorithm/DataStructure/ThisIsD&A/LinkedList/DList/main.c
@@ -0,0 +1,41 @@
+#include "DList.h"
+
+int main(void) {
+    int i = 0;
+    int count = 0;
+    Node* list = NULL;
+    Node* current = NULL;
+    Node* newNode = NULL;
+
+    for(i = 0; i < 5; i++) {
+        newNode = create_node(i);
+        append_node(&list, newNode);
+    }
+
+    count = get_node_count(list);
+    for(i = 0; i < count; i++) {
+        current = get_node_at(list, i);
+        printf("List[%d] : %d\n", i, current->data);
+    }
+
+    printf("\nInserting 3000 After [2]... \n\n");
+    current = get_node_at(list, 2);
+    newNode = create_node(3000);
+    insert_after(current, newNode);
+
+    count = get_node_count(list);
+    for(i = 0; i < count; i++) {
+        current = get_node_at(list, i);
+        printf("List[%d] : %d\n", i, current->data);
+    }
+
+    printf("\nDestroy all node\n");
+    for(i = 0; i < count; i++) {
+        current = get_node_at(list, 0);
+        if(current != NULL) {
+            remove_node(&list, current);
+            destroy_node(current);
+        }
+    }
+    return 0;
+}
