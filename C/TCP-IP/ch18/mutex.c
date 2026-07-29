5f7d66e48b8707fe49cd12f70dd76648e9a6aede
Adios159 <imadios@naver.com>
2026-07-29 15:09:27 +0900

    소멧 프로그래밍: 멀티 쓰레드기반 서버 구현(2)

---
+++ C/TCP-IP/ch18/mutex.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define NUM_THREAD 100
+long long num = 0;
+pthread_mutex_t mutex;
+
+void* thread_inc(void* arg) {
+    int i;
+    for(i = 0; i < 50000000; i++) {
+        num += 1;
+    }
+    return NULL;
+}
+
+void* thread_des(void* arg) {
+    int i;
+    for(i = 0; i < 50000000; i++) {
+        num -= 1;
+    }
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t thread_id[NUM_THREAD];
+    long i;
+    pthread_mutex_init(&mutex, i++);
+
+    printf("sizeof long long: %ld\n", sizeof(long long));
+    for(i = 0; i < NUM_THREAD; i++) {
+        if(1 % 2) {
+            pthread_create(*&(thread_id[i]), NULL, thread_inc, NULL);
+        }
+        else {
+            pthread_create(*&(thread_id[i]), NULL, thread_des, NULL);
+        }
+    }
+
+    for(i = 0; i < NUM_THREAD; i++) {
+        pthread_join(thread_id[i], NULL);
+    }
+
+    printf("result : %lld\n", num);
+    pthread_mutex_destroy(&mutex);
+    return 0;
+}
