c1b6587f599cb53095b75052256af20b5397f250
Adios159 <imadios@naver.com>
2026-07-29 16:43:25 +0900

    div3 1107(a, b): 정수론의 중요성을 깨닫게 됨

---
+++ C++/PS/contest/codeforces/Div3/1107/a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- 타입 별칭 ----------
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pair<int, int>> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+// ---------- 매크로 ----------
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+// ---------- 상수 ----------
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const int MOD = 1e9 + 7;
+const double PI = 3.14159265358979;
+
+int main() {
+    fastio;
+    ll N;
+    cin >> N;
+
+    while(N--) {
+        ll x, y;
+        cin >> x >> y;
+        if(x % y == 0) {
+            cout << "yes\n";
+        }
+        else {
+            cout << "no\n";
+        }
+    }
+    return 0;
+}
