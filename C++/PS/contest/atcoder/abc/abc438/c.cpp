7557ba440b50fe3cca008270ef1cd92a6839a201
Adios159 <imadios@naver.com>
2026-07-30 16:49:51 +0900

    abc438(a~c): 무난하게 풀었다

---
+++ C++/PS/contest/atcoder/abc/abc438/c.cpp
@@ -0,0 +1,53 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    // ---------- 타입 별칭 ----------
+    typedef long long ll;
+    typedef unsigned long long ull;
+    typedef pair<int, int> pii;
+    typedef pair<ll, ll> pll;
+    typedef vector<int> vi;
+    typedef vector<ll> vl;
+    typedef vector<pair<int, int>> vpii;
+    typedef vector<pll> vpll;
+    typedef vector<vi> vvi;
+    typedef vector<vl> vvl;
+
+    // ---------- 매크로 ----------
+    #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+    #define all(x) (x).begin(), (x).end()
+    #define rall(x) (x).rbegin(), (x).rend()
+    #define sz(x) (int)(x).size()
+
+    // ---------- 상수 ----------
+    const int INF = 1e9;
+    const ll LLINF = 4e18;
+    const int MOD = 1e9 + 7;
+    const double PI = 3.14159265358979;
+
+    int main() {
+        fastio;
+        ll N, A;
+        cin >> N;
+        stack<pair<ll, ll>> nums;
+        while(N--) {
+            cin >> A;
+            if(nums.empty() == false && nums.top().first == A) {
+                nums.top().second++;
+            }
+            else {
+                nums.push(make_pair(A, 1));
+            }
+            if(nums.top().second == 4) {
+                nums.pop();
+            }
+        }
+        ll cnt = 0;
+        while(!nums.empty()) {
+            auto temp = nums.top();
+            cnt += temp.second;
+            nums.pop();
+        }
+        cout << cnt << '\n';
+        return 0;
+    }
