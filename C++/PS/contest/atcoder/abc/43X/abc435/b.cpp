415100a9794f96f022ab024cf03b69c76b910022
Adios159 <imadios@naver.com>
2026-08-07 23:22:46 +0900

    abc435(a, b, c): 그냥저냥 무난했다

---
+++ C++/PS/contest/atcoder/abc/43X/abc435/b.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- 타입 별칭 ----------
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pair<int, int>> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+// ---------- 매크로 ----------
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+// ---------- 상수 ----------
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const int MOD = 1e9 + 7;
+const double PI = 3.14159265358979;
+
+int main() {
+    fastio;
+    int N;
+    int cnt = 0, sum = 0;
+    cin >> N;
+    vi num(N);
+    bool is_divided = false;
+    for(int i = 0; i < N; i++) {
+        cin >> num[i];
+    }
+    for(int i = 0; i < N; i++) {
+        for(int j = i + 1; j < N; j++) {
+            sum = 0;
+            is_divided = false;
+            for(int k = i; k <= j; k++) {
+                sum += num[k];
+            }
+            for(int k = i; k <= j; k++) {
+                if(sum % num[k] == 0) {
+                    is_divided = true;
+                    break;
+                }
+            }
+            if(is_divided == false) {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << '\n';
+    return 0;
+}
